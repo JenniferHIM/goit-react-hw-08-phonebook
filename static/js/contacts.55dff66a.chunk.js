44ed08978589a07f2b167c86908a474edabd9767
Evgeniya Obernikhina <jenniferhim1201@gmail.com>
2021-07-23 20:15:30 +0300

    Updates

---
+++ static/js/contacts.55dff66a.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-08-phonebook"]=this["webpackJsonpgoit-react-hw-08-phonebook"]||[]).push([[0],{159:function(e,c,s){"use strict";s.r(c);var t=s(36),o=s(2);c.default=function(){return Object(o.jsxs)("div",{className:"myPhonebook",children:[Object(o.jsx)("h2",{className:"bigHeader",children:"My phonebook"}),Object(o.jsx)(t.a,{}),Object(o.jsx)(t.c,{}),Object(o.jsx)("h2",{className:"smallHeader",children:"My contacts:"}),Object(o.jsx)(t.b,{})]})}}}]);
+//# sourceMappingURL=contacts.55dff66a.chunk.js.map
